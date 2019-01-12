3db1afd4cfbcad56d5b196e18a74f3debc3085cf
noliaki <noliaki@gmail.com>
2019-01-13 01:35:49 +0900

    :tada: init commit

---
+++ nuxt.config.js
@@ -0,0 +1,64 @@
+const TerserPlugin = require('terser-webpack-plugin')
+
+module.exports = {
+  srcDir: 'src/',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    htmlAttrs: {
+      lang: 'ja'
+    },
+    title: 'mokumoku-1',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'mokumoku-1' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  transition: {
+    name: 'hoge',
+    mode: 'out-in'
+  },
+  css: [
+    '@/assets/style.css'
+  ],
+  router: {
+    base: '/'
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  generate: {
+    dir: 'public'
+  },
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isClient) {
+        if (!config.optimization.minimizer) {
+          config.optimization.minimizer = []
+        }
+
+        config.optimization.minimizer.push(
+          new TerserPlugin({
+            terserOptions: {
+              compress: {
+                drop_console: process.env.NODE_ENV === 'production'
+              }
+            }
+          })
+        )
+      }
+    }
+  }
+}
